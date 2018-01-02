50178828a288aa2cc8088bcdfd13e1fb8463a0ba
Ethan Roberts <ethan.reid.roberts@gmail.com>
2018-01-02 10:56:26 -0500

    added groupadmin attribute to members table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102153307) do
+ActiveRecord::Schema.define(version: 20180102155503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20180102153307) do
     t.boolean "group_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "groupadmin", default: false
   end
 
   create_table "users", force: :cascade do |t|
