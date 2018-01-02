57a75a564b04593be704cffae0d4930a50e022de
Ethan Roberts <ethan.reid.roberts@gmail.com>
2018-01-02 10:35:40 -0500

    added public attribute to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207172619) do
+ActiveRecord::Schema.define(version: 20180102153307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171207172619) do
     t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "public", default: false
   end
 
   create_table "groups", force: :cascade do |t|
