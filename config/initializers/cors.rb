565286f920d48f6b7b4b96842db8ff84ece1019e
Ethan Roberts <ethan.reid.roberts@gmail.com>
2018-02-17 09:09:05 -0500

    heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://shacc-ed3ef.firebaseapp.com/'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
